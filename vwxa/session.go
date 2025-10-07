94558a9354a733b7d23c29d563c7ce7d4fd47db9
tiltwind <tiltwind@163.com>
2025-10-07 15:32:27 +0800

    add unionid for session

--- vwxa/session.go
+++ vwxa/session.go
@@ -33,6 +33,7 @@ const (
 type SessionResponse struct {
 	OpenID     string `json:"openid"`
 	SessionKey string `json:"session_key"`
+	UnionID    string `json:"unionid"`
 	ErrCode    int    `json:"errcode"`
 	ErrMsg     string `json:"errmsg"`
 }
